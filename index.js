6ad9d1d538a43b2f795723df9c35c1c33124dee4
taauntik <devauntik@gmail.com>
2021-12-11 20:07:08 +0600

    writeStream using pipe method

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const fs = require('fs');
 const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`);
 const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
 
-ourReadStream.on('data', (chunk) => {
-    ourWriteStream.write(chunk);
-});
+// ourReadStream.on('data', (chunk) => {
+//     ourWriteStream.write(chunk);
+// });
+
+// using pipe for write stream
+ourReadStream.pipe(ourWriteStream);
