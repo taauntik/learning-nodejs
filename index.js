0eee49e4eae459a33e55d94e6b89d48a8215a390
taauntik <devauntik@gmail.com>
2021-12-11 20:02:24 +0600

    writeStream is done

--- index.js
+++ index.js
@@ -1,46 +1,8 @@
-const http = require('http');
+const fs = require('fs');
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.write(`
-            <html>
-                <head>
-                    <title></title>
-                </head>
-                <body>
-                    <form method="post" action="/process">
-                        <input name="message" />
-                    </form>
-                </body>
-            </html>
-        `);
-        res.end();
-    } else if (req.url === '/process' && req.method === 'POST') {
-        const body = [];
-        // req.on('data', (chunk) => {
-        //     body.push(chunk);
-        // });
-        req.on('end', () => {
-            console.log('stream finished');
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            res.write('Hello');
-            res.end();
-        });
-    } else {
-        res.write('404 not found');
-    }
+const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`);
+const ourWriteStream = fs.createWriteStream(`${__dirname}/output.txt`);
+
+ourReadStream.on('data', (chunk) => {
+    ourWriteStream.write(chunk);
 });
-
-server.listen(5000);
-console.log('server is listening on port 5000');
-
-// const fs = require('fs');
-
-// const ourReadStream = fs.createReadStream(`${__dirname}/bigdata.txt`);
-
-// ourReadStream.on('data', (chunk) => {
-//     console.log(chunk);
-// });
-
-// console.log('Hello');
