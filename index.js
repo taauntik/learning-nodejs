9fc3625ea98c2ed92c6474406cc10207b64a8c5e
taauntik <devauntik@gmail.com>
2021-12-11 12:18:38 +0600

    learned about path module of nodejs

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('done editor and project setup for learning node js');
+// importing the path module
+const path = require('path');
+
+const myPath = '/home/auntik/programming/github/learning-nodejs/index.js';
+
+// This is gonna give us the basename of the path
+console.log(path.basename(myPath)); // return index.js
+
+// This is gonna give us the full path of the directory
+console.log(path.dirname(myPath)); // return /home/auntik/programming/github/learning-nodejs
+
+// This is gonna give us the extension name of the path
+console.log(path.extname(myPath)); // return .js
+
+// This is gonna return an object
+console.log(path.parse(myPath)); // return { root, dir, base, ext, name }
